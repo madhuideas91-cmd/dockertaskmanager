4a5102432be44a65afb80aa86a2361ccacab0343
madhuideas91-cmd <madhu.ideas91@gmail.com>
2026-02-20 22:18:29 +0530

    correction

---
+++ taskManagement_Backend/api-gateway/src/main/java/com/example/taskmanager/api_gateway/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.taskmanager.api_gateway.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        List<String> origins = Arrays.asList("http://77.37.47.79:3000");
+        corsConfig.setAllowedOrigins(origins);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
